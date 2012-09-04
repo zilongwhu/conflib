3db5fd4aa728cc8a0ed9da09c83de22dc6793781
zilong.whu <zilong.whu@1c7832a4-a5af-cb24-30cb-29addb9573ba>
2012-09-04 14:37:00 +0000

    add buffer_t

---
+++ trunk/Makefile
@@ -0,0 +1,4 @@
+tester: src/*.cpp
+	g++ -g -Wall -Iinclude src/*.cpp -o tester
+clean:
+	rm tester
---
+++ trunk/src/main.cpp
@@ -0,0 +1,33 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2012年09月02日 00时31分38秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Zilong.Zhu (), zilong.whu@gmail.com
+ *        Company:  edu.whu
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include "buffer.h"
+
+int main(int argc, char *argv[])
+{
+	buffer_t buf;
+	buf+=10;
+	buf.put_char(' ');
+	buf+=10.12345;
+	buf.put_char(' ');
+	for (int i=0; i<16;++i)
+		buf.print_hex(i);
+	printf("%s\n", buf.c_str());
+	return 0;
+}
